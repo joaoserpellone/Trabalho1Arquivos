a62f7903415927d8c3b07e0cd12bf2aa93050211
Joao Serpellone <108192121+joaoserpellone@users.noreply.github.com>
2023-05-16 21:21:23 -0300

    Add files via upload
    Pepe's final version

--- deletFromWhere.h
+++ deletFromWhere.h
@@ -4,8 +4,19 @@
     #include <stdio.h>
     #include "registroCabecalho.h"
     
+    /*
+    *   Funcao que recebe sequencia de campos a serem
+    *   buscados para remocao de registro, eh usada para
+    *   remocao de indices (caso tenha) do tipo int
+    */
     void deletFromWhereInt(FILE *arqBin, CABECALHO *cabecalho, char *campoIndexado, char *nomeArqIndice, int n);
 
+
+    /*
+    *   Funcao que recebe sequencia de campos a serem
+    *   buscados para remocao de registro, eh usada para
+    *   remocao de indices (caso tenha) do tipo string
+    */
     void deletFromWhereStr(FILE *arqBin, CABECALHO *cabecalho, char *campoIndexado, char *nomeArqIndice, int n);    
 
 #endif
--- selectFromWhere.h
+++ selectFromWhere.h
@@ -3,8 +3,21 @@
 
     #include <stdio.h>
     #include "registroCabecalho.h"
-    
+
+    /*
+    *   Funcao que realiza sequencia de m buscas
+    *   de registros, a busca pode ser indexada (tipo int) 
+    *   e com mais campos ou linear, 
+    *   diretamente no arquivo binario 
+    */
     void selectFromWhereInt(FILE *arqBin, CABECALHO *cabecalho, char *campoIndexado, char *nomeArqIndice, int n);
 
+
+    /*
+    *   Funcao que realiza sequencia de m buscas
+    *   de registros, a busca pode ser indexada (tipo string) 
+    *   e com mais campos ou linear, 
+    *   diretamente no arquivo binario 
+    */
     void selectFromWhereStr(FILE *arqBin, CABECALHO *cabecalho, char *campoIndexado, char *nomeArqIndice, int n);
 #endif
